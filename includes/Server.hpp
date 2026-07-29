e59ec6b1fa3f2ce42c2d8bf79af83fa59c52651c
Muhsin <muhsin.kulbak@gmail.com>
2026-07-29 18:56:01 +0300

    public belirteçe sahip olup public olarak  kullanılmayan fonksiyonlar private yapıldı

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -20,28 +20,27 @@ private:
 
     std::set<Socket*>               _listenSockets;
     std::set<Client*>               _clientSockets;
-
+    std::vector<struct epoll_event> _events;            // epoll_wait'in dolduracağı vector
+    std::time_t                     _lastTimeoutCheck;
     int                             _epollFd;
     
-    std::vector<struct epoll_event> _events; // epoll_wait'in dolduracağı vector
-    std::time_t                     _lastTimeoutCheck;
-
     Server(const Server& other);
     Server& operator=(const Server& other);
 
-public:
-    Server();
-    ~Server();
-
-    // Bunların static ya da private olma durumlarını değerlendir.
-    void    init(const ConfigParser& config); // port numarası alır ve masterSocket'e verir
-    void    run();
     void    acceptNewConnection(Socket* masterSocket);
     void    handleClientReceive(Client* client, epoll_event *event);  // EPOLLIN: istemciden veri alma
     void    handleClientSend(Client* client, epoll_event *event);     // EPOLLOUT: istemciye veri gönderme
     void    deleteClient(Client* client);
     void    checkExpiredSockets();
     void    registerHandler(EpollHandler* socket);
+    
+public:
+
+    Server();
+    ~Server();
+
+    void    init(const ConfigParser& config); // Config dosyasını alıp socketleri (ip:port) açar
+    void    run();
 
 };
 
